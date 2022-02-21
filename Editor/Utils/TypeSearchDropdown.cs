83e710a098c972c0918aaef5a40f4172bb8d254b
Arnaud Begue <abegue@persistant.fr>
2022-02-21 16:51:12 +0100

    feat(type-search-dropdown): rework of the Type Search Dropdown to be done async

--- Editor/Utils/TypeSearchDropdown.cs
+++ Editor/Utils/TypeSearchDropdown.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Unity.EditorCoroutines.Editor;
+using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
@@ -16,12 +18,16 @@ namespace AUE
             public ETypeUsageFlag UsageFlags { get; set; }
         }
 
+        private const int NumOperationsAllowedPerFrame = 1000;
         private static Dictionary<Assembly, Type[]> _typesCache = null;
+        private static FieldInfo _getWindowInstanceFI = null;
 
         private Settings _customSettings = null;
+        private EditorCoroutine _currentBuildRootCoroutine = null;
+        private int _numOperationsDone;
 
         public event Action<Type> OnTypeSelected;
-
+        private EditorWindow _currentWindow;
 
         public TypeSearchDropdown(AdvancedDropdownState state, Settings customSettings = null)
             : base(state)
@@ -32,66 +38,111 @@ namespace AUE
 
         protected override AdvancedDropdownItem BuildRoot()
         {
-            var root = new AdvancedDropdownItem("root");
-            CacheTypesIFN();
-            foreach (var kv in _typesCache)
+            var root = new AdvancedDropdownItem("Type");
+
+            if (_currentBuildRootCoroutine != null)
             {
-                var assemblyItem = new AdvancedDropdownItem(kv.Key.GetName().Name) { id = kv.Key.GetHashCode() };
-                int count = kv.Value.Length;
-                for (int i = 0; i < count; ++i)
-                {
-                    var type = kv.Value[i];
-                    assemblyItem.AddChild(new AdvancedDropdownItem(type.FullName) { id = type.GetHashCode() });
-                }
-                root.AddChild(assemblyItem);
+                EditorCoroutineUtility.StopCoroutine(_currentBuildRootCoroutine);
             }
-            root.AddSeparator();
-            root.AddChild(new AdvancedDropdownItem("Undefined") { id = 0 });
+
+            _currentBuildRootCoroutine = EditorCoroutineUtility.StartCoroutine(BuildRootAsync(root), this);
             return root;
         }
 
-        private void CacheTypesIFN()
+        private IEnumerator BuildRootAsync(AdvancedDropdownItem root)
         {
-            if (_typesCache != null)
-            {
-                return;
-            }
-
+            _currentWindow = GetCurrentWindow();
+            _numOperationsDone = 0;
             _typesCache = new Dictionary<Assembly, Type[]>();
             var aueSettings = AUESettingsProvider.GetOrCreateSettings<AUESettings>();
-            var assemblyReferences = aueSettings.TypesAssemblies;
-            foreach (var assemblyRef in assemblyReferences)
+            foreach (var assemblyRef in aueSettings.TypesAssemblies)
             {
-                var assembly = assemblyRef.Assembly;
-                if (assembly != null && !_typesCache.ContainsKey(assembly))
+                yield return BuildTypesForAssembly(root, assemblyRef);
+            }
+            root.AddSeparator();
+            root.AddChild(new AdvancedDropdownItem("Undefined") { id = 0 });
+
+            _currentWindow.Repaint();
+
+            _currentBuildRootCoroutine = null;
+        }
+
+        private IEnumerator BuildTypesForAssembly(AdvancedDropdownItem root, AssemblyReference assemblyRef)
+        {
+            var assemblyNode = new AdvancedDropdownItem(assemblyRef.AssemblyShortName) { id = assemblyRef.GetHashCode() };
+
+            if (_typesCache.TryGetValue(assemblyRef.Assembly, out Type[] types))
+            {
+                if (types.Length == 0)
                 {
-                    if (_customSettings != null)
+                    yield break;
+                }
+
+                for (int i = 0; i < types.Length; ++i)
+                {
+                    assemblyNode.AddChild(new AdvancedDropdownItem(types[i].Name) { id = types[i].GetHashCode() });
+                    ++_numOperationsDone;
+
+                    if (HasDoneEnoughOperationsThisFrame())
                     {
-                        Type[] types = GetConstraintTypesFromAssembly(assembly, _customSettings);
-                        if (types != null && types.Length > 0)
-                        {
-                            _typesCache.Add(assembly, types);
-                        }
-                    }
-                    else
-                    {
-                        _typesCache.Add(assembly, assembly.GetTypes());
+                        _currentWindow.Repaint();
+                        yield return null;
                     }
                 }
+                root.AddChild(assemblyNode);
+            }
+            else
+            {
+                IEnumerable<Type> enumTypes;
+                if (_customSettings != null)
+                {
+                    enumTypes = GetConstraintTypesEnumeratorFromAssembly(assemblyRef.Assembly, _customSettings);
+                }
+                else
+                {
+                    enumTypes = assemblyRef.Assembly.GetTypes();
+                }
+
+                foreach (var type in enumTypes)
+                {
+                    assemblyNode.AddChild(new AdvancedDropdownItem(type.Name) { id = type.GetHashCode() });
+                    ++_numOperationsDone;
+
+                    if (HasDoneEnoughOperationsThisFrame())
+                    {
+                        _currentWindow.Repaint();
+                        yield return null;
+                    }
+                }
+
+                // Check if there is at least one child
+                if (assemblyNode.children.Any())
+                {
+                    root.AddChild(assemblyNode);
+                }
+
+                _typesCache.Add(assemblyRef.Assembly, enumTypes.ToArray());
             }
         }
 
-        private Type[] GetConstraintTypesFromAssembly(Assembly assembly, Settings settings)
+        private bool HasDoneEnoughOperationsThisFrame()
+            => (_numOperationsDone > 0 && _numOperationsDone % NumOperationsAllowedPerFrame == 0);
+
+        private IEnumerable<Type> GetConstraintTypesEnumeratorFromAssembly(Assembly assembly, Settings settings = null)
         {
             Type[] types = assembly.GetTypes();
+            if (settings == null)
+            {
+                return types;
+            }
+
             return types
-                .Where((t) 
+                .Where((t)
                 => (settings.ConstraintType == null || settings.ConstraintType.IsAssignableFrom(t))
                 && (!t.IsAbstract || settings.UsageFlags.HasFlag(ETypeUsageFlag.Abstract))
                 && (!t.IsInterface || settings.UsageFlags.HasFlag(ETypeUsageFlag.Interface))
                 && (!t.IsClass || settings.UsageFlags.HasFlag(ETypeUsageFlag.Class))
-                && (!t.IsValueType || settings.UsageFlags.HasFlag(ETypeUsageFlag.Struct)))
-                .ToArray();
+                && (!t.IsValueType || settings.UsageFlags.HasFlag(ETypeUsageFlag.Struct)));
         }
 
         protected override void ItemSelected(AdvancedDropdownItem item)
@@ -108,7 +159,6 @@ namespace AUE
             }
             foreach (var kv in _typesCache)
             {
-                var assemblyItem = new AdvancedDropdownItem(kv.Key.GetName().Name) { id = kv.Key.GetHashCode() };
                 int count = kv.Value.Length;
                 for (int i = 0; i < count; ++i)
                 {
@@ -120,6 +170,15 @@ namespace AUE
                 }
             }
             return null;
+        }
+
+        private EditorWindow GetCurrentWindow()
+        { 
+            if (_getWindowInstanceFI == null)
+            {
+                _getWindowInstanceFI = typeof(TypeSearchDropdown).GetField("m_WindowInstance", BindingFlags.NonPublic | BindingFlags.Instance);
+            }
+            return (EditorWindow)_getWindowInstanceFI.GetValue(this);
         }
     }
 }
