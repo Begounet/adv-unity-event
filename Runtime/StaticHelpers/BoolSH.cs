ebdce976409917ce5350895a265c2669eed7b8f6
Arnaud Begue <abegue@persistant.fr>
2022-08-24 15:10:54 +0200

    feat(static): add first static helper "BoolSH", allowing to invert bool value

---
+++ Runtime/StaticHelpers/BoolSH.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AUE.StaticHelpers
+{
+    public static class BoolSH
+    {
+        public static bool Invert(bool value) => !value;
+    }
+}
