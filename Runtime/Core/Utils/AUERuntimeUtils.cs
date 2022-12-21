3d69e7009be93a7431b1497d0211f77c9563fc6f
Arnaud Begue <abegue@apperture.fr>
2022-12-21 17:28:24 +0100

    fix(aue): use UnityEngine.Object.m_CachedPtr instead of m_InstanceID to determine if an UnityEngine.Object is valid or not. Was not working when building on il2cpp

--- Runtime/Core/Utils/AUERuntimeUtils.cs
+++ Runtime/Core/Utils/AUERuntimeUtils.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
@@ -7,12 +8,12 @@ namespace AUE
 {
     public static class AUERuntimeUtils
     {
-        private static readonly FieldInfo InstanceIDFieldInfo = typeof(Object).GetField("m_InstanceID", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static readonly FieldInfo CachedPtrFI = typeof(UnityEngine.Object).GetField("m_CachedPtr", BindingFlags.Instance | BindingFlags.NonPublic);
 
-        public static bool IsUnityObjectValid(Object obj)
+        public static bool IsUnityObjectValid(UnityEngine.Object obj)
         {
-            int instanceID = (int)InstanceIDFieldInfo.GetValue(obj);
-            return instanceID != 0;
+            IntPtr instanceID = (IntPtr)CachedPtrFI.GetValue(obj);
+            return instanceID != IntPtr.Zero;
         }
     }
 }
